cd07d2de047c3d13794a4471fa918d042bc2ac9f
freshvictory <justin.renjilian@gmail.com>
2019-04-13 17:49:31 -0400

    Home controller with ascii art

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+    render :plain => "
+                   _          
+    ,_   _  ,_    //       ,_ 
+  _/ (__(/__/_)__(/__(_/__/ (_
+           /         _/_      
+          /         (/        
+"
+  end
+end
