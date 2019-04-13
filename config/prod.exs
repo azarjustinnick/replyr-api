6dbe9852e0642ebe688c359b5fa12e5837f1d847
freshvictory <justin.renjilian@gmail.com>
2019-04-13 15:12:16 -0400

    Remove static cache reference

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,6 @@ config :replyr_api, ReplyrApiWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "replyr.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
