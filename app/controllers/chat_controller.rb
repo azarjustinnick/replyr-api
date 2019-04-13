7c50ae7dba279df6a28d7ebafeb41033f1d854c7
freshvictory <justin.renjilian@gmail.com>
2019-04-13 18:58:50 -0400

    Mock message data

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -6,7 +6,26 @@ class ChatController < ApplicationController
   # GET channel
 
   def channel
-    render json: { "name" => params[:name], "messages" => [] }
+    render json: {
+      "name" => params[:name],
+      "messages" => [
+        {
+          "username" => "adarr",
+          "text" => "Message #1",
+          "timestamp" => "1555196237"
+        },
+        {
+          "username" => "nvladimiroff",
+          "text" => "Message #2",
+          "timestamp" => "1555197237"
+        },
+        {
+          "username" => "jrenjilian",
+          "text" => "Message #3",
+          "timestamp" => "1555198237"
+        }
+      ]
+    }
   end
-  
+
 end
