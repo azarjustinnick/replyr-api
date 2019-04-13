3a5fb4f87cb086b707bd6b36e38c00e6069ad576
freshvictory <justin.renjilian@gmail.com>
2019-04-13 19:06:51 -0400

    Use different time format and add post method

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,7 +3,7 @@ require 'mongo'
 class ChatController < ApplicationController
   # client = Mongo::Client.new('mongodb://replyr-db.documents.azure.com:10255')
 
-  # GET channel
+  # GET channel/:name
 
   def channel
     render json: {
@@ -12,20 +12,26 @@ class ChatController < ApplicationController
         {
           "username" => "adarr",
           "text" => "Message #1",
-          "timestamp" => "1555196237"
+          "timestamp" => "2019-04-13T23:01:43.025Z"
         },
         {
           "username" => "nvladimiroff",
           "text" => "Message #2",
-          "timestamp" => "1555197237"
+          "timestamp" => "2019-04-13T23:02:43.025Z"
         },
         {
           "username" => "jrenjilian",
           "text" => "Message #3",
-          "timestamp" => "1555198237"
+          "timestamp" => "2019-04-13T23:03:43.025Z"
         }
       ]
     }
+  end
+
+  # POST channel/:name/message
+
+  def create_message
+    render status: 201
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get 'chat/channel/:name' => 'chat#channel'
+  post 'chat/channel/:name/message' => 'chat#create_message'
   
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
